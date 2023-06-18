eb2a51cf3b41a3cd2a920b4301a4aef2591b09e0
Bleu <65407864+BernatSR@users.noreply.github.com>
2023-06-18 23:55:55 +0200

    Modif readme et ajout rapport en pdf dans la section rapport

---
+++ analyse/Main.java
@@ -0,0 +1,33 @@
+package exercice;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //calcule le temps d'exécution et la mémoire utilisée
+        long startTime = System.nanoTime();
+        long beforeUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+        String texte = "Il fait beau aujourd’hui comme en aout";
+        List<Character> ordre = List.of('f', 'I', 'z', 'u', 'k', 'a', 'b', 'o');
+        System.out.println(Exercice.solution(texte, ordre));
+        // output: [classer, texte, exemple, a, de]
+        long endTime = System.nanoTime();
+        long afterUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+        long duration = (endTime - startTime);
+        long actualMemUsed = afterUsedMem - beforeUsedMem;
+        double durationMillis = (double) duration / 1_000_000.0;
+        System.out.println("Temps d'exécution : " + durationMillis + " ms");
+        System.out.println("Memoire utilisée : " + actualMemUsed + " octets");
+
+    }
+}
+
+/*
+ * Exemple d’input
+ * texte = "Il fait beau aujourd’hui comme en aout"
+ * 
+ * ordre = ['f', 'I', 'z', 'u', 'k', 'a', 'b', 'o']
+ * 
+ * Exemple d’ouput
+ * ["fait", "Il", "aujourd", "aout", "beau", "hui", "comme", "en"]
+ */
